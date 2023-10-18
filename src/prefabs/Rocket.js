455b0c29cef9d5bcf7025614f7e685a94cb45fda
axhiggin <axhiggin@ucsc.edu>
2023-10-18 14:31:45 -0700

    done!

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
       scene.add.existing(this);
       this.isFiring = false;
       this.moveSpeed = 2;
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update() {
@@ -18,9 +19,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
                 this.x += this.moveSpeed;
             }
         }
-        // fire
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        
+        // fire button
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
         //if fired move rocket up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
