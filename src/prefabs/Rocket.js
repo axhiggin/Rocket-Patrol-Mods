e00b21fb4ad3a9a1bcec856006172bf433fb23ce
axhiggin <axhiggin@ucsc.edu>
2023-10-21 19:35:23 -0700

    added controllable rocket

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -11,7 +11,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
     }
 
     update() {
-        if(!this.isFiring){
+        if(true){
             if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
             }
